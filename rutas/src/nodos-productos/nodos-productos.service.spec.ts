f2f2e1ad9ceed0dc7da6676422e36f010fa8dee3
saidydnnsoft <said.nader@ydn.com.co>
2025-03-28 17:37:51 -0500

    se configuran los tests [desplegar usuarios]

--- rutas/src/nodos-productos/nodos-productos.service.spec.ts
+++ rutas/src/nodos-productos/nodos-productos.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NodosProductosService } from './nodos-productos.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { NodoProductoEntity } from './entities/nodo-producto.entity';
 
 describe('NodosProductosService', () => {
   let service: NodosProductosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NodosProductosService],
+      providers: [
+        NodosProductosService,
+        {
+          provide: getRepositoryToken(NodoProductoEntity),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<NodosProductosService>(NodosProductosService);
--- rutas/src/tipos-rutas/tipos-rutas.service.spec.ts
+++ rutas/src/tipos-rutas/tipos-rutas.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TiposRutasService } from './tipos-rutas.service';
+import { TipoRutaEntity } from './entities/tipo-ruta.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('TiposRutasService', () => {
   let service: TiposRutasService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TiposRutasService],
+      providers: [
+        TiposRutasService,
+        {
+          provide: getRepositoryToken(TipoRutaEntity),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<TiposRutasService>(TiposRutasService);
