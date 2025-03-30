532967ff9be66224cb5e59472bd713a2f347d916
Fabian Orozco <fabian.orozco@mercadolibre.com.co>
2025-03-29 20:20:05 -0500

    init users

--- ccp-web/src/app/app.routes.ts
+++ ccp-web/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
 import {AuthGuard} from './guards/auth.guard';
+import { UsersComponent } from './pages/users/users.component';
+import { ManageUsersComponent } from './pages/users/manage-users/manage-users.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'manage-users', component: ManageUsersComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }
 ];
--- ccp-web/src/app/pages/users/users.component.ts
+++ ccp-web/src/app/pages/users/users.component.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { ManageUsersComponent } from './manage-users/manage-users.component';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { TableModule } from 'primeng/table';
+
+interface User {
+  name: string;
+  mail: string;
+  role: string;
+  state: string;
+}
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [CardModule, DividerModule, TableModule, ButtonModule, ManageUsersComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
+
 export class UsersComponent {
+  users: User[] = [
+    {
+      name: 'Laptop ThinkPad',
+      mail: 'mail@createComponent.com',
+      role: 'Administrador',
+      state: 'Unidad'
+    },
+    {
+      name: 'Wireless Mouse',
+      mail: 'mail@createComponent.com',
+      role: 'Administrador',
+      state: 'Unidad'
+    },
+    {
+      name: 'USB-C Cable',
+      mail: 'mail@createComponent.com',
+      role: 'Administrador',
+      state: 'Unidad'
+    },
+    {
+      name: 'Monitor 27"',
+      mail: 'mail@createComponent.com',
+      role: 'Administrador',
+      state: 'Unidad'
+    }
+  ];
+
+  constructor(
+    //private modalService: ModalService
+  ) {}
+
+  openModal() {
+    console.log('openModal');
+    //this.modalService.openModal();
+  }
 
 }
