65feb9163b6fcd3c581c40402f8fab7a7616bf2b
saidydnnsoft <said.nader@ydn.com.co>
2025-04-05 08:08:11 -0500

    se implementa form de inventario

--- ccp-web/src/environments/environment.ts
+++ ccp-web/src/environments/environment.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  apiUrl: 'https://orquestador-1083277898027.us-central1.run.app',
-  apiProductos: 'https://productos-1083277898027.us-central1.run.app'
-  
+  apiUrl: "https://orquestador-1083277898027.us-central1.run.app",
 };
