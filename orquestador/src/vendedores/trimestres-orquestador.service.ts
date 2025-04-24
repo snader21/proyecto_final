eed4af278c25fbf5bc83b51ee78cb26799373a32
Fabian Orozco <fabian.orozco@mercadolibre.com.co>
2025-04-24 00:20:00 -0500

    Metodo del orquestdor

---
+++ orquestador/src/vendedores/trimestres-orquestador.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, HttpException } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class TrimestresOrquestadorService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async getTrimestresPorAno(ano: number) {
+    try {
+      const api = this.configService.get<string>('URL_VENDEDORES') || 'http://localhost:3000';
+      const { data } = await firstValueFrom(this.httpService.get(`${api}/trimestres/${ano}`));
+      return data;
+    } catch (error) {
+      throw new HttpException('Error consultando trimestres en vendedores', 502);
+    }
+  }
+}
