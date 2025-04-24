77dee8cb2519fde6e9d403fb9d46a787b1469209
Fabian Orozco <fabian.orozco@mercadolibre.com.co>
2025-04-24 00:34:01 -0500

    Update trimestres-orquestador.service.ts

--- orquestador/src/vendedores/trimestres-orquestador.service.ts
+++ orquestador/src/vendedores/trimestres-orquestador.service.ts
@@ -13,7 +13,7 @@ export class TrimestresOrquestadorService {
   async getTrimestresPorAno(ano: number) {
     try {
       const api = this.configService.get<string>('URL_VENDEDORES') || 'http://localhost:3000';
-      const { data } = await firstValueFrom(this.httpService.get(`${api}/trimestres/${ano}`));
+      const { data } = await firstValueFrom(this.httpService.get(`${api}/plan-ventas/trimestres/${ano}`));
       return data;
     } catch (error) {
       throw new HttpException('Error consultando trimestres en vendedores', 502);
