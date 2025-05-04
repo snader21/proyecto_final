83619c1cbbb67e1ad5d5a1c1b8a4a85e021004a1
Andres Martinez <af.martinezc1@uniandes.edu.co>
2025-05-03 22:35:57 -0500

    Captura de video funcionando en emulador y web

--- ccp-mobile/capacitor.config.ts
+++ ccp-mobile/capacitor.config.ts
@@ -1,9 +1,14 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ccp-mobile',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+    hostname: 'localhost',
+		iosScheme: "https",
+  }
 };
 
 export default config;
