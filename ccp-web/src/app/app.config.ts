6d1b40a6b98c3ed3bc839051fcd32ac5bc1d7e07
Andres Martinez <af.martinezc1@uniandes.edu.co>
2025-05-10 13:27:03 -0500

    test con hasch

--- ccp-web/src/app/app.config.ts
+++ ccp-web/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
@@ -29,7 +29,7 @@ const MyPreset = definePreset(Aura, {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([AuthInterceptor])),
     providePrimeNG({
